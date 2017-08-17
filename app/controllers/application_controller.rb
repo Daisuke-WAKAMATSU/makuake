87f3e97323494cf70948aabd23c11ac5951e562f
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-17 18:34:11 +0900

    Root edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
+
+
+
