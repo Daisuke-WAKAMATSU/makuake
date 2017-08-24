4419773e4137c732ed6fa210c159a1e4c4f9f1e5
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-24 16:34:56 +0900

    Project view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
   end
 
   def after_sign_out_path_for(resource)
