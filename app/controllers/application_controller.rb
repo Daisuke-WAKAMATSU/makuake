578131b574d4a72b880a2e0de3e86e3384ba71d7
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-29 13:39:35 +0900

    Planner modal edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :create_planner
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :name])
@@ -10,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  def create_planner
+    @planner = Planner.new
   end
 end
 
