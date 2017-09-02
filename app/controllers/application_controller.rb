ca704dd5f2a7b875215784a8b55754421827f9ac
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-09-02 12:19:58 +0900

    Devise rooting edit
    ログイン後の遷移先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :create_planner
+  after_action  :store_location
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
+  end
+
+  def store_location
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath !~ Regexp.new("\\A/users/password.*\\z") &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
   end
 
   def after_sign_out_path_for(resource)
